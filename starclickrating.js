2b8adcc4361c119af25a595c9a70bec09fdd7874
cornicopia77 <hutchinsonhere@gmail.com>
2026-05-19 13:03:59 -0400

    Update starclickrating.js

--- starclickrating.js
+++ starclickrating.js
@@ -1,26 +1,32 @@
 
-const StarRating = () => {
-    const [rating, setRating] = useState(0);
-    const [hover, setHover] = useState(0);
+import React, { useState } from 'react';
+ 
+ const StarRating = () => {
+     const [rating, setRating] = useState(0);
+     const [hover, setHover] = useState(0);
+ 
+     return (
+         <div className="star-rating">
+            {[...Array(5)].map((_, index) => {
+                const starValue = index + 1;
+                return (
+                    <button
+                        key={starValue}
+                        type="button"
+                        className={starValue <= (hover || rating) ? 'on' : 'off'}
+                        onClick={() => setRating(starValue)}
+                        onMouseEnter={() => setHover(starValue)}
+                        onMouseLeave={() => setHover(0)}
+                        aria-label={`Rate ${starValue} star${starValue > 1 ? 's' : ''}`}
+                    >
+                        <span className="star">&#9733;</span>
+                    </button>
+                );
+            })}
+         </div>
+     );
+ };
 
-    return (
-        <div className="star-rating">
-            {[...Array(5)].map((star, index) => {   
-        const starValue = index + 1;
-        return (
-            <button
-            key={starValue}
-            className={starValue <= (hover || rating) ? 'on' : 'off'}
-            onClick={() => setRating(starValue)}
-            onMouseEnter={() => setHover(starValue)}        
-            onMouseLeave={() => setHover(0)}
-            >
-                <span className="star">&#9733;</span>       
-            </button>
-        );      
-
-})}
-        </div>
-    );
-};
-
+export default StarRating;
+ 
+ 
